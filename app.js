eeea4947b7f9669aa33436f4f7b3816b565ae44d
wangyuzju <wangyu0248@gmail.com>
2015-02-10 20:14:36 +0800

    更新 api 名字

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/ghook', require('./routes/ghook'));
+app.use('/git_hook', require('./routes/git_hook'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
