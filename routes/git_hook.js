31b9ac6cc48038207bae06bf5e3bdebea8383e9f
wangyuzju <wangyu0248@gmail.com>
2015-02-10 20:20:05 +0800

    update test

--- routes/git_hook.js
+++ routes/git_hook.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/callback', function(req, res, next) {
+    //res.render('index', { title: 'Express' });
+    res.json(req.body);
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/callback', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-    res.json(req.body);
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
