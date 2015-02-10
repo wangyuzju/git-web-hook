dd37268c7ea77745f7e3c04bb606f0a450c69515
wangyuzju <wangyu0248@gmail.com>
2015-02-10 21:20:48 +0800

    能接收到  bitbucket hooks,  需要进行适配

---
+++ dev/nobone-sync.coffee
@@ -0,0 +1,7 @@
+module.exports =
+  local_dir: './'
+  remote_dir: '/home/wy/www/git-web-hook'
+  host: 'hellofe.com'
+  port: 8345
+  pattern: '**/*'
+  polling_interval: 800
--- routes/git_hook.js
+++ routes/git_hook.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/callback', function(req, res, next) {
+router.post('/callback', function(req, res, next) {
     //res.render('index', { title: 'Express' });
-    res.json(req.body);
+    console.log(req.body)
+
+    res.json({errCode: 22000});
 });
 
 module.exports = router;
